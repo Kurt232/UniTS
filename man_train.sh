f7d8b88649ad4060a2a34a1a8ea38e2276556dba
duwenjie <wduaj@cse.ust.hk>
2024-11-14 00:15:19 +0800

    eval: 400 epochs, 1e-3 lr

--- man_train.sh
+++ man_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GPU=0
+GPU=2,3
 LOAD_PATH=""
 DATA_CONFIG="/data/wjdu/data4/realworld/realworld_10_thigh_TRAIN.json /data/wjdu/data4/realworld/realworld_10_thigh_TEST.json"
-OUTPUT_DIR="/data/wjdu/realworld_thigh/UniTS_HEAD_1_10"
-MASTER_PORT=2112
+OUTPUT_DIR="/data/wjdu/Nov12/UniTS_HEAD_4_10"
+MASTER_PORT=3112
 
 mkdir -p "$OUTPUT_DIR"
 
@@ -12,9 +12,9 @@ echo "Training on GPU: $GPU with MASTER_PORT: $MASTER_PORT"
 echo "Data config: $DATA_CONFIG"
 echo "Output directory: $OUTPUT_DIR"
 
-CUDA_VISIBLE_DEVICES=$GPU python -u -m torch.distributed.launch --master_port=$MASTER_PORT --nproc_per_node=1 --use_env \
+CUDA_VISIBLE_DEVICES=$GPU python -u -m torch.distributed.launch --master_port=$MASTER_PORT --nproc_per_node=2 --use_env \
  train.py --data_config $DATA_CONFIG --load_path "$LOAD_PATH" --batch_size 512 \
- --epochs 200 --warmup_epochs 20 --blr 1e-4 --min_lr 1e-6 --weight_decay 5e-6 \
+ --epochs 400 --warmup_epochs 20 --blr 1e-3 --min_lr 1e-6 --weight_decay 5e-6 \
  --output_dir "$OUTPUT_DIR" \
  --seed 42 \
 #  > "$OUTPUT_DIR/output.log" 2>&1
